61738a433508a2cb42751a07110d970531312f03
N4NO <nathan.rouse56734@gmail.com>
2022-03-05 15:39:32 +0000

    test

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const mongoose = require('mongoose')
+const mongoose = require('mongodb')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
