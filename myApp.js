b9a07dda8bfd4e2dd4f6c4a8e825c909f47ca321
N4NO <nathan.rouse56734@gmail.com>
2022-03-05 16:11:00 +0000

    uppercase S

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new Schema({
-    name: { type: string, required: true },
+    name: { type: String, required: true },
     age: Number,
     favoriteFoods: [String]
 })
