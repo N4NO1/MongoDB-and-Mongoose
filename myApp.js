55b527816c839c4fe43c9fa44056cd6aadf87d29
N4NO <nathan.rouse56734@gmail.com>
2022-03-05 17:19:46 +0000

    test

--- myApp.js
+++ myApp.js
@@ -45,7 +45,7 @@ const findPeopleByName = (personName, done) => {
 
 const findOneByFood = (food, done) => {
 
-    Person.findOne({ food: food }, function (err, personFound) {
+    Person.findOne({ favoriteFoods: food }, function (err, personFound) {
         if (err) return console.log(err)
         done(null , personFound)
     })
