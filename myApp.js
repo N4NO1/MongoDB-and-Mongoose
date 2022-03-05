780c7267fcfee4a546c0d91a51c6f215e3ccb09f
N4NO <nathan.rouse56734@gmail.com>
2022-03-05 15:41:00 +0000

    changed to mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const mongoose = require('mongodb')
+const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
